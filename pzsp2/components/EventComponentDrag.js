d14e76abb3dc54f50dc3829fa3dace32416b3b5c
mtaudul <mikit2002@wp.pl>
2024-06-06 11:12:40 +0200

    Price != 0

--- pzsp2/components/EventComponentDrag.js
+++ pzsp2/components/EventComponentDrag.js
@@ -4,7 +4,9 @@ import styles from '../styles/EventComponentDrag.module.css';
 const EventComponentDrag = ({ event }) => (
   <div className={styles.eventContainer}>
     <div className={styles.eventTitle}>{event.title}</div>
-    <div className={styles.eventPrice}>Cena: {event.lastMarketPrice}</div>
+    {event.lastMarketPrice !== 0 && (
+      <div className={styles.eventPrice}>Cena: {event.lastMarketPrice}</div>
+    )}
   </div>
 );
 
